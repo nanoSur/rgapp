2cd4d2b9e37a77b0f5c5afbc8fd3ce9f6d28b7b6
nanoSur <sunaina2roy@gmail.com>
2019-07-25 12:00:28 +0530

    form updated

--- src/components/Contact/ContactPage.js
+++ src/components/Contact/ContactPage.js
@@ -1,32 +1,55 @@
 import React, { Component } from "react"
+import Heading from "../Reuseable/Heading"
 
 export default class ContactPage extends Component {
   render() {
     return (
       <section className="py-3">
-        <div className="container">
-          <form>
-            <div className="form-group row">
-              <label
-                for="staticEmail"
-                class="control-label col-sm-2 text-secondary"
-              >
-                Email
-              </label>
-              <div className="col-sm-10">
-                <input
-                  type="text"
-                  className="form-control-plaintext"
-                  action="/new.php"
-                  placeholder="drop email here"
-                />
-              </div>
-              <div className="col-sm-offset-2 col-sm-10 justify-content my-3 d-flex ">
-                <button type="submit" class="btn btn-info">
-                  Contact us
-                </button>
-              </div>
+        <Heading title="Contact us" />
+        <div className="col-10 col-sm-8 mx-auto">
+          <form
+            action="https://formspree.io/sunaina2roy@gmail.com"
+            method="POST"
+          >
+            <div className="form-group">
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Your Name"
+                className="form-control"
+              />
             </div>
+            <div className="form-group">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Your E-Mail"
+                className="form-control"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                name="mobile"
+                id="mobile"
+                placeholder="Your Phone"
+                className="form-control"
+              />
+            </div>
+            <div className="form-group">
+              <textarea
+                type="text"
+                name="description"
+                id="description"
+                placeholder="Your message"
+                className="form-control"
+              />
+            </div>
+            <button type="submit" className="btn btn-outline-info btn-block">
+              submit
+            </button>
           </form>
         </div>
       </section>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,21 +6,19 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import HeroSection from "../components/Reuseable/HeroSection"
 import InfoBlock from "../components/Reuseable/InfoBlock"
-import DualInfoBlock from "../components/Reuseable/DualInfoBlock"
-import ServiceCourse from "../components/Services/ServiceCourse"
 import ContactPage from "../components/Contact/ContactPage"
 
-const ServicesPage = ({ data }) => (
+const contactPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
 
     <HeroSection
       img={data.img.childImageSharp.fluid}
-      title="I write Code"
+      title="Be our part"
       subtitle=""
       heroclass="about-background"
     />
-    <InfoBlock heading="Conatct Us" />
+    <InfoBlock heading="How can we help?" />
     <ContactPage />
   </Layout>
 )
@@ -29,7 +27,7 @@ export const query = graphql`
   {
     img: file(relativePath: { eq: "contact.png" }) {
       childImageSharp {
-        fluid(maxWidth: 350, maxHeight: 350) {
+        fluid {
           ...GatsbyImageSharpFluid_tracedSVG
         }
       }
@@ -37,4 +35,4 @@ export const query = graphql`
   }
 `
 
-export default ServicesPage
+export default contactPage
